598d82292e2f3823808b1b32e0e288f011e1a9c5
amandaandrus <aandrus1@byupathway.edu>
2025-07-09 18:56:16 -0600

    C# Exercise2 Week 1

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -4,6 +4,40 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        Console.Write("What is your grade percentage? ");
+        string userInput = Console.ReadLine();
+        int gradePercentage = int.Parse(userInput);
+
+        string letter = "";
+        if (gradePercentage >= 90)
+        {
+            letter = "A";
+        }
+        else if (gradePercentage >= 80)
+        {
+            letter = "B";
+        }
+        else if (gradePercentage >= 70)
+        {
+            letter = "C";
+        }
+        else if (gradePercentage >= 60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+
+        if (gradePercentage >= 70)
+        {
+            Console.WriteLine("Congradulations! You passed the class!");
+        }
+        else
+        {
+            Console.WriteLine("You didn't pass. Use what you learned and try again!");
+        }
+        Console.WriteLine($"You grade is {letter}");
     }
 }
